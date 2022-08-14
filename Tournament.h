684afa0096c8767a6857711e043c2eca3eef2fd7
a1844950 <100755253+a1844950@users.noreply.github.com>
2022-08-14 19:08:07 +0800

    Update Tournament.h

--- Tournament.h
+++ Tournament.h
@@ -14,7 +14,7 @@ class Tournament {
         //int setPlayers();
         Player* fivePlays(Player* player1, Player* player2);
 
-        Player * run(array<Player *, 8>  competitors);
+        Player * run(array<Player *, 8>  competitors)
         std::string compete();
         ~Tournament();
 
