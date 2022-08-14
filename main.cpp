517f9384c95474368942285cc4c9ed1cea7ffc2e
a1844950 <100755253+a1844950@users.noreply.github.com>
2022-08-14 18:58:50 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,74 +1,53 @@
 #include <iostream>
 #include <string>
-#include <sstream>
+#include <array>
+
 #include "Player.h"
-#include "Toolbox.h"
-#include "Bureaucrat.h"
+
+#include "Human.h"
+#include "Computer.h"
+#include "Referee.h"
+#include "RandomComputer.h"
 #include "Avalanche.h"
+#include "Bureaucrat.h"
+#include "Toolbox.h"
+
 #include "Crescendo.h"
 #include "PaperDoll.h"
 #include "FistfullODollars.h"
+
 #include "Tournament.h"
-#include "Referee.h"
-#include "RandomComputer.h"
-#include "Human.h"
+
 using namespace std;
 
-int main(int argc, char* argv[])
-{
-string computers;
-getline(cin, computers); 
-istringstream computerStream(computers); 
-Tournament newTourn;
-do
-{
-string word;
-computerStream >> word; 
 
-if(word == "Avalanche")
+int main()
 {
-Player * newLanche = new Avalanche().;
-newTourn.addPlayers(newLanche);
-}
-if(word == "Bureaucrat")
-{
-Player * newCrat = new Bureaucrat().;
-newTourn.addPlayers(newCrat);
-}
-if(word == "Toolbox")
-{
-Player * newBox = new Toolbox().;
-newTourn.addPlayers(newBox);
+    Human *H1 = new Human();
+    Human *H2 = new Human();
 
-}
-if(word == "Crescendo")
-{
-Player * newCres = new Crescendo().;
-newTourn.addPlayers(newCres);
-}
-if(word == "PaperDoll")
-{
-Player * newDoll = new PaperDoll().;
-newTourn.addPlayers(newDoll);
-}
-if(word == "FistfullODollars")
-{
-Player * newFist = new FistfullODollars().;
-newTourn.addPlayers(newFist);
-}
-if(word == "RandomComputer")
-{
-Player * newrnd = new RandomComputer().;
-newTourn.addPlayers(newrnd);
-}
-if(word == "Human")
-{
-Player * newh = new Human().;
-newTourn.addPlayers(newh);
-}
-} while (computerStream); 
-
-cout << newTourn.compete();
+/*    Computer *C = new Computer();
+    Referee *R = new Referee();
+    RandomComputer *RC = new RandomComputer();
+    PaperDoll *P = new PaperDoll();*/
 
 
+    Avalanche *A = new Avalanche();
+    Bureaucrat *B = new Bureaucrat();
+    Bureaucrat *B2 = new Bureaucrat();
+    Toolbox *T = new Toolbox();
+    Toolbox *T2 = new Toolbox();
+    Crescendo *Co = new Crescendo();
+    Crescendo *Co2 = new Crescendo();
+    FistfullODollars *F = new FistfullODollars();
+
+
+    std::array<Player*, 8>competitors = {A,B,B2,T,T2,Co,Co2,F};
+    Player* winner = Tournament().run(competitors);
+
+    //cout << *winner << endl;
+
+
+
+    return 0;
 }
